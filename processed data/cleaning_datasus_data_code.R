b8c20ea5f8faee0f4449cfba1127dd08600944a5
Matheus Rezende Dias <matheusrezdias@gmail.com>
2024-05-24 18:49:22 -0300

    Cleaning datasus code

---
+++ processed data/cleaning_datasus_data_code.R
@@ -0,0 +1,41 @@
+rm(list = ls())
+library(tidyverse)
+library(plm)
+library(readr)
+library(PNSIBGE)
+library(mfx)
+
+#############################
+# DATA CLEANING
+#############################
+
+# sih sus
+read_process_sih <- function(file_path, year){
+  readxl::read_xlsx(
+    path = file_path,
+    sheet = year,
+    skip = 9,
+    col_names = c("nome_codigo", "internacoes", "custo_internacao",
+                  "dias_permanencia", "obitos","taxa_mortalidade"),
+    col_types = c("text", "numeric", "numeric", "numeric",
+                  "numeric","numeric")) %>% 
+    mutate(ano = as.numeric(year),
+           codigo_mun = as.numeric(substr(nome_codigo,1,6)))
+}
+
+df_sihsus_2014 <- read_process_sih("sih_datasus.xlsx", "2014")
+df_sihsus_2015 <- read_process_sih("sih_datasus.xlsx", "2015")
+df_sihsus_2016 <- read_process_sih("sih_datasus.xlsx", "2016")
+df_sihsus_2017 <- read_process_sih("sih_datasus.xlsx", "2017")
+df_sihsus_2018 <- read_process_sih("sih_datasus.xlsx", "2018")
+df_sihsus_2019 <- read_process_sih("sih_datasus.xlsx", "2019")
+df_sihsus_2020 <- read_process_sih("sih_datasus.xlsx", "2020")
+df_sihsus_2021 <- read_process_sih("sih_datasus.xlsx", "2021")
+
+df_sihsus <- bind_rows(df_sihsus_2014,df_sihsus_2015,df_sihsus_2016,df_sihsus_2017,
+                       df_sihsus_2018,df_sihsus_2019,df_sihsus_2020,df_sihsus_2021)
+
+rm(df_sihsus_2014,df_sihsus_2015,df_sihsus_2016,df_sihsus_2017,
+   df_sihsus_2018,df_sihsus_2019,df_sihsus_2020,df_sihsus_2021)
+
+df <- df_snis %>% left_join(df_sihsus, by = c("codigo_mun", "ano"))
