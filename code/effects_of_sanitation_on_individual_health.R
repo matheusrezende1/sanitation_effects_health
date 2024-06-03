568f8cde41ee16a8d3c4429d60b2b9dafbe8d378
Matheus Rezende Dias <matheusrezdias@gmail.com>
2024-06-03 17:34:53 -0300

    update

---
+++ code/effects_of_sanitation_on_individual_health.R
@@ -0,0 +1,97 @@
+rm(list = ls())
+library(tidyverse)
+library(plm)
+library(readr)
+library(PNSIBGE)
+library(mfx)
+library(readxl)
+
+df <- read_csv("./processed data/pns_tratada.csv")
+
+#################################################################################
+# PLOTS
+#################################################################################
+
+prop_all <- df %>% filter(!is.na(rede_esgoto)) %>% 
+  group_by(rede_esgoto) %>% 
+  summarise(n = n()) %>% 
+  mutate(prop_all = n/sum(n))
+
+prop_sick <- df %>% filter(!is.na(rede_esgoto)) %>%
+  filter(acamado_causa_hidrica == 1) %>% 
+  group_by(rede_esgoto) %>% 
+  summarise(n = n()) %>% 
+  mutate(prop_sick = n/sum(n))
+
+prop_all %>% left_join(prop_sick, by = "rede_esgoto") %>% 
+  pivot_longer(cols = starts_with("prop"),
+               names_to = "Grupo",
+               values_to = "Proporcao") %>% 
+  ggplot(aes(x = factor(rede_esgoto,
+                        labels = c("Sem Saneamento", "Com Saneamento")),
+             y = Proporcao, fill = Grupo)) +
+  geom_bar(stat = "identity", position = position_dodge(width = 0.7), width = 0.6) +
+  scale_fill_brewer(palette = "Dark2") +
+  labs(x = "Saneamento Básico", y = "Proporção", fill = "Grupo",
+       title = "Proporção de pessoas com e sem saneamento, geral e acamados") +
+  theme_minimal()
+  
+  
+#################################################################################
+# REGRESSIONS
+#################################################################################
+
+
+ols_all <- lm(acamado_causa_hidrica ~ rede_esgoto + agua_canalizada +
+                tem_rede_agua + educ + trab + idade +
+                homem, data = df)
+summary(ols_all)
+
+ols_all_old <- lm(acamado_causa_hidrica ~ rede_esgoto + agua_canalizada + educ + trab +
+                    homem, data = df)
+summary(ols_all_old)
+
+ols_all_afastado <- lm(afastado_causa_hidrica ~ rede_esgoto + educ + trab +
+                         homem, data = df)
+summary(ols_all_afastado)
+
+ols_norte <- lm(acamado_causa_hidrica ~ rede_esgoto + agua_canalizada + educ + trab +
+                  homem, data = df %>% filter(regiao == "Norte"))
+summary(ols_norte)
+
+ols_nordeste <- lm(acamado_causa_hidrica ~ rede_esgoto + agua_canalizada + educ + trab +
+                     homem, data = df %>% filter(regiao == "Nordeste"))
+summary(ols_nordeste)
+
+ols_sul <- lm(acamado_causa_hidrica ~ rede_esgoto + agua_canalizada + educ + trab +
+                homem, data = df %>% filter(regiao == "Sul"))
+summary(ols_sul)
+
+ols_sudeste <- lm(acamado_causa_hidrica ~ rede_esgoto + agua_canalizada + educ + trab +
+                    homem, data = df %>% filter(regiao == "Sudeste"))
+summary(ols_sudeste)
+
+ols_centro_oeste <- lm(acamado_causa_hidrica ~ rede_esgoto + agua_canalizada + educ + trab +
+                         homem, data = df %>% filter(regiao == "Centro-Oeste"))
+summary(ols_centro_oeste)
+
+
+#################################################################################
+# ROBUSTNESS
+#################################################################################
+
+logit <- glm(acamado_causa_hidrica ~ rede_esgoto + agua_canalizada +
+               tem_rede_agua + educ + trab + idade +
+               homem, family = binomial(link="logit"),
+             data = df)
+summary(logit)
+
+logitmfx(acamado_causa_hidrica ~ rede_esgoto + agua_canalizada +
+           tem_rede_agua + educ + trab + idade +
+           homem,data = df )
+
+probitmfx(acamado_causa_hidrica ~ rede_esgoto + agua_canalizada +
+            tem_rede_agua + educ + trab + idade +
+            homem,data = df )
+
+
